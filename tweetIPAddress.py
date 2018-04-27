ba2dfe9dab0be6361c1f9706ed132598143e495c
Kevin McCullen <kmccullen@users.noreply.github.com>
2018-04-26 22:21:13 -0400

    Fixed one bug, added new code
    (1) Ham-handed attempt to use an exception to handle sequence number for first time run. Had wrong exception specified. Not sure if there's an OS dependency, but replaced FileNotFoundError with IOError.
    
    (2) Added contents of /etc/hostname to the tweet.

--- tweetIPAddress.py
+++ tweetIPAddress.py
@@ -1,22 +1,26 @@
 from twitter import Twitter, OAuth
 import subprocess
-from datetime import datetime
+import sys
 
 try:
     fileVar = open("/home/pi/lastTweetSeq.txt", "r")
     lastTweet = fileVar.read()
     lastTweetSeq = int(lastTweet) + 1
     fileVar.close()
-except FileNotFoundError:
+except IOError:
     lastTweetSeq = 0
+    
 fileVar = open("/home/pi/lastTweetSeq.txt", "w")
 fileVar.write(str(lastTweetSeq) + "\n")
+fileVar.close()
+
+with open("/etc/hostname") as fileVar:
+    hostName = fileVar.read()[:-1]
 
 myIP = subprocess.Popen("hostname -I", shell=True, stdout=subprocess.PIPE).stdout.read()
 myIPstr = str(myIP.strip())
 myIPstr = myIPstr.replace("'","").replace("b","")
-print(str(datetime.now()) + " " + myIPstr)
-tweet = 'Raspberry Pi connected at ' + myIPstr + " (" + str(lastTweetSeq) + ")"
+tweet = hostName + " connected at " + myIPstr + " (" + str(lastTweetSeq) + ")"
 
 token="xxxx"
 token_secret="xxxx"
@@ -24,5 +28,4 @@ consumer_key="xxxx"
 consumer_secret="xxxx"
 
 twitter = Twitter(auth=OAuth(token, token_secret, consumer_key, consumer_secret))
-twitter.statuses.update(status=tweet)
 twitter.direct_messages.new(user="uuuu", text=tweet)
